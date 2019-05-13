f4c0e194cf4b39f819288f87d9096796a6ae6bec
Esdeasu <47320193+Esdeasu@users.noreply.github.com>
2019-05-13 22:24:13 +0300

    Create Lab4.serv.py

---
+++ Lab4.serv.py
@@ -0,0 +1,26 @@
+import socket
+import re
+
+bred = socket.socket()
+bred.bind(('', 7890))
+bred.listen(1)
+serv, addr = bred.accept()
+while True:
+    print('Ожидание соединения...')
+    data = serv.recv(34000)
+    print('Соединение установлено.')
+    data = data.decode()
+    data = re.findall(r'\d+', data)
+    a = float(data[0])
+    b = float(data[1])
+    c = float(data[2])
+    d = float(data[3])
+    k = float(data[4])
+
+    res = ((a**2-b**3 - c**3*a**2)*(b-c+c*(k-d/b**3)) - (k/b - k/a)*c)**2 - 2000
+    res = 'Ответ = ' + str(res)
+    if not data:
+        w = 'wtf men'
+        serv.send(w.encode())
+    break
+serv.send(res.encode())
